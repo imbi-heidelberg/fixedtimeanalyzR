6b0fdb4bda91b5c3c48e8fed97c820fbf5932a01
Lukas D. Sauer <sauer@imbi.uni-heidelberg.de>
2021-09-10 16:02:08 +0200

    Starting to implement tests using testthat

---
+++ tests/testthat/test-survivaltestpvalues.R
@@ -0,0 +1,6 @@
+context("survival test p-values")
+
+test_that("the p-values for naive.test, logtra.test, clog.test, asinsqrt.test and
+          logit.test are equal to the corresponding tests from other packages"){
+
+}
